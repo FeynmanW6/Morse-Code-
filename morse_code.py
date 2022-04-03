7be9cb12c55cacb9da094d0083abfb4f9038e464
FeynmanW6 <72461989+FeynmanW6@users.noreply.github.com>
2022-04-03 23:56:56 +0200

    Add files via upload

---
+++ morse_code.py
@@ -0,0 +1,40 @@
+
+answer = input("Do you wish to encode or decode? ")
+
+dic = { 'A':'.-', 'B':'-...',
+        'C':'-.-.', 'D':'-..', 'E':'.',
+        'F':'..-.', 'G':'--.', 'H':'....',
+        'I':'..', 'J':'.---', 'K':'-.-',
+        'L':'.-..', 'M':'--', 'N':'-.',
+        'O':'---', 'P':'.--.', 'Q':'--.-',
+        'R':'.-.', 'S':'...', 'T':'-',
+        'U':'..-', 'V':'...-', 'W':'.--',
+        'X':'-..-', 'Y':'-.--', 'Z':'--..',
+        '1':'.----', '2':'..---', '3':'...--',
+        '4':'....-', '5':'.....', '6':'-....',
+        '7':'--...', '8':'---..', '9':'----.',
+        '0':'-----', ', ':'--..--', '.':'.-.-.-',
+        '?':'..--..', '/':'-..-.', '-':'-....-',
+        '(':'-.--.', ')':'-.--.-' , ' ' :' '}
+
+dic_reverse = {v: k for k, v in dic.items()}
+
+def encode():
+
+    txt = input("Insert message: ")
+    for char in txt:
+        print(dic[char.upper()])
+
+def decode():
+
+    txt = input("Insert message: ")
+    for char in txt.split():
+        print(dic_reverse[char])
+
+
+if answer == "encode":
+    encode()
+elif answer == "decode":
+    decode()
+else:
+    print("Your input was not recognized.")
